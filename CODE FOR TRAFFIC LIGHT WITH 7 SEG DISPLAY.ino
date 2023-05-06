e94397e3cf5dff87315de94c2ce01e46dc8f206f
sushantshekhar-tech <116714886+sushantshekhar-tech@users.noreply.github.com>
2023-05-06 13:27:50 +0530

    first commit

---
+++ CODE FOR TRAFFIC LIGHT WITH 7 SEG DISPLAY.ino
@@ -0,0 +1,335 @@
+int up=13;
+int ru=12;
+int rl=11;
+int lo=10;
+int ll=9;
+int lu=8;
+int mi=7;
+int green=4;
+int orange=3;
+int red=2;
+int del=200;
+
+void setup() {
+
+  // put your setup code here, to run once:
+pinMode(up,OUTPUT);
+pinMode(ru,OUTPUT);
+pinMode(rl,OUTPUT);
+pinMode(lo,OUTPUT);
+pinMode(ll,OUTPUT);
+pinMode(lu,OUTPUT);
+pinMode(mi,OUTPUT);
+pinMode(green,OUTPUT);
+pinMode(orange,OUTPUT);
+pinMode(red,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(red,HIGH);
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, LOW);
+  delay(del);
+
+
+  digitalWrite(up, LOW);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, LOW);
+  digitalWrite(mi, LOW);
+  delay(del);
+  
+
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, LOW);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, LOW);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, LOW);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+
+
+  digitalWrite(up, LOW);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, LOW);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, LOW);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, LOW);
+  delay(del);
+
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+
+digitalWrite(red,LOW);
+
+
+
+  digitalWrite(orange,HIGH);
+
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, LOW);
+  delay(del);
+
+  digitalWrite(up, LOW);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, LOW);
+  digitalWrite(mi, LOW);
+  delay(del);
+  
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, LOW);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, LOW);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, LOW);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+  digitalWrite(up, LOW);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, LOW);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, LOW);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, LOW);
+  delay(del);
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+
+digitalWrite(orange,LOW);
+
+
+  digitalWrite(green,HIGH);
+
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, LOW);
+  delay(del);
+
+  digitalWrite(up, LOW);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, LOW);
+  digitalWrite(mi, LOW);
+  delay(del);
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, LOW);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, LOW);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, LOW);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+
+  digitalWrite(up, LOW);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, LOW);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, LOW);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, LOW);
+  delay(del);
+
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+
+digitalWrite(green,LOW);
+delay(del);
+}
