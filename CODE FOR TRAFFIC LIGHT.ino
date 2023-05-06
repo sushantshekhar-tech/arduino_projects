29ba4a0b7f5a4b109f84d5ea92ca22477c537d6c
sushantshekhar-tech <116714886+sushantshekhar-tech@users.noreply.github.com>
2023-05-06 13:27:01 +0530

    first commit

---
+++ CODE FOR TRAFFIC LIGHT.ino
@@ -0,0 +1,383 @@
+int up=13;
+int ru=12;
+int rl=11;
+int lo=10;
+int ll=9;
+int lu=8;
+int mi=7;
+int green=4;
+int orange=3;
+int red=2;
+int del=500;
+
+void setup() {
+
+  // put your setup code here, to run once:
+pinMode(up,OUTPUT);
+pinMode(ru,OUTPUT);
+pinMode(rl,OUTPUT);
+pinMode(lo,OUTPUT);
+pinMode(ll,OUTPUT);
+pinMode(lu,OUTPUT);
+pinMode(mi,OUTPUT);
+pinMode(green,OUTPUT);
+pinMode(orange,OUTPUT);
+pinMode(red,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+zero();
+one();
+two();
+three();
+four();
+five();
+six();
+seven();
+eight();
+nine();
+}
+{
+  digitalWrite(red,HIGH);
+void zero(){
+  
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+}
+void one(){
+  digitalWrite(up, LOW);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, LOW);
+  digitalWrite(mi, LOW);
+  delay(del);
+  
+}
+void two(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, LOW);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, LOW);
+  digitalWrite(mi, HIGH);
+  delay(del);
+}
+void three(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, LOW);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+}
+void four(){
+  digitalWrite(up, LOW);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+}
+void five(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, LOW);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+}
+void six(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, LOW);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+}
+void seven(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, LOW);
+  delay(del);
+
+}
+void eight(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+}
+void nine(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+}
+digitalWrite(red,LOW);
+delay(del);
+}
+{
+  digitalWrite(orange,HIGH);
+
+void zero(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+}
+void one(){
+  digitalWrite(up, LOW);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, LOW);
+  digitalWrite(mi, LOW);
+  delay(del);
+  
+}
+void two(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, LOW);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, LOW);
+  digitalWrite(mi, HIGH);
+  delay(del);
+}
+void three(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, LOW);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+}
+void four(){
+  digitalWrite(up, LOW);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+}
+void five(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, LOW);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+}
+void six(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, LOW);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+}
+void seven(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, LOW);
+  delay(del);
+
+}
+void eight(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+}
+void nine(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+}
+digitalWrite(orange,LOW);
+delay(del);
+}
+{
+  digitalWrite(green,HIGH);
+
+void zero(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+}
+void one(){
+  digitalWrite(up, LOW);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, LOW);
+  digitalWrite(mi, LOW);
+  delay(del);
+  
+}
+void two(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, LOW);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, LOW);
+  digitalWrite(mi, HIGH);
+  delay(del);
+}
+void three(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, LOW);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+}
+void four(){
+  digitalWrite(up, LOW);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+}
+void five(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, LOW);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+}
+void six(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, LOW);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+}
+void seven(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, LOW);
+  delay(del);
+
+}
+void eight(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, HIGH);
+  digitalWrite(ll, HIGH);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+}
+void nine(){
+  digitalWrite(up, HIGH);
+  digitalWrite(ru, HIGH);
+  digitalWrite(rl, HIGH);
+  digitalWrite(lo, LOW);
+  digitalWrite(ll, LOW);
+  digitalWrite(lu, HIGH);
+  digitalWrite(mi, HIGH);
+  delay(del);
+
+}
+digitalWrite(green,LOW);
+delay(del);
+}
